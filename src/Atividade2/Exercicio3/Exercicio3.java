9fbc176754b9ed40622e23d451ae1b706d1aba8a
Anna Laura Junges <anna_lauraj@yahoo.com.br>
2021-06-20 14:21:38 -0300

    Exercício 3

--- src/Atividade2/Exercicio3/Exercicio3.java
+++ src/Atividade2/Exercicio3/Exercicio3.java
@@ -1,8 +1,30 @@
 package Atividade2.Exercicio3;
 
+import java.util.Scanner;
+
 public class Exercicio3 {
 
+/*Escreva um algoritmo que leia um vetor de 10 elementos inteiros. Encontre e mostre o menor elemento e a sua
+posição.*/
+
     public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        int[] vet = new int[10];
+        int posicao = 300, menor = 300;
+
+        System.out.println("Digite 10 elementos para o vetor: ");
+        for (int i = 0; i < 10; i++) {
+            vet[i] = input.nextInt();
+        }
+        for (int i = 0; i < 10; i++) {
+            if (vet[i] < menor) {
+                menor = vet[i];
+                posicao = i;
+            }
+        }
+        System.out.println("\nO menor elemento do vetor eh " + menor + " localizado na posicao [" + posicao + "]");
 
     }
 }
