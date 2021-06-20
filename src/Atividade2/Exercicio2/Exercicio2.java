fd9dd81da3aeaa560d8aee2d9e11b73be0aefa6d
Anna Laura Junges <anna_lauraj@yahoo.com.br>
2021-06-20 14:19:17 -0300

    Exercício 2

--- src/Atividade2/Exercicio2/Exercicio2.java
+++ src/Atividade2/Exercicio2/Exercicio2.java
@@ -1,8 +1,31 @@
 package Atividade2.Exercicio2;
 
+import java.util.Scanner;
+
+/*Escreva um algoritmo que leia dois vetores de 10 posições e faça a multiplicação dos elementos de mesmo índice,
+colocando o resultado em um terceiro vetor. Mostre o vetor resultante.*/
+
+
 public class Exercicio2 {
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int[] vetA = new int[10], vetB = new int[10], vetC = new int[10];
+
+        System.out.println("Digite os valores do VETOR A:");
+        for (int i = 0; i < 10; i++) {
+            vetA[i] = input.nextInt();
+        }
+        System.out.println("\nDigite os valores do VETOR B:");
+        for (int i = 0; i < 10; i++) {
+            vetB[i] = input.nextInt();
+        }
+        System.out.println("\nVETOR A X VETOR B = VETOR C");
+        for (int i = 0; i < 10; i++) {
+            vetC[i] = vetA[i] * vetB[i];
+            System.out.println(vetA[i] + " X " + vetB[i] + " = " + vetC[i]);
+        }
 
     }
 }
